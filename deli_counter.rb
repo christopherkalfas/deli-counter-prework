fffa46cabba21123fd70b3180b674e3b4af2ccdf
Christopher Kalfas <chriskalfas@yahoo.com>
2019-03-11 18:58:02 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,5 +2,5 @@ def line
   if number_of_people == 0 
     puts  "The line is currently empty."
   else number_of_people > 0 
-    puts "You sre currently number #{number_of_people +1}."
+    puts "The line is currently: #{number_of_people +1}."
 end 
