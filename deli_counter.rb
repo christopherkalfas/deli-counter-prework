ffefeaaab597f85303b6771c6aabdd980a04ee4e
Christopher Kalfas <chriskalfas@yahoo.com>
2019-03-11 19:17:26 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,12 +1,12 @@
 katz_deli = []
 
 def line(katz_deli)
-  if katz_deli < 1  
+  if katz_deli.length == 0  
     puts "The line is currently empty."
   end 
-  else katz_deli >= 1 
-    katz_deli.each_with_index do |name, index|
-      puts "The current line is: #{index + 1 } #{name}"
+  else 
+    line_prefix = "The line is currently:"
+    number_of_people = 0
   end 
   
 end
