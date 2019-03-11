51fec556445fce22b29bc73a2b873fa86ec382bc
Christopher Kalfas <chriskalfas@yahoo.com>
2019-03-11 19:02:54 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,8 +1,4 @@
-def line(katz) 
-  number_of_people = []
-  if number_of_people == 0 
-    puts  "The line is currently empty."
-  else number_of_people > 0 
-    puts "The line is currently: #{number_of_people +1}."
-  end 
+def line(katz_deli)
+  
+  
 end
